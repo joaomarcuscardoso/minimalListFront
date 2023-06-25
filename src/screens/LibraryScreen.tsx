f6ba91d8985edd5ea10cda470347df655029f332
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2023-06-25 14:18:06 -0300

    Ajustando Library

--- src/screens/LibraryScreen.tsx
+++ src/screens/LibraryScreen.tsx
@@ -8,10 +8,12 @@ import { useCookies } from "react-cookie"
 import { AiFillDelete } from "react-icons/ai"
 import { Button } from "components/Button"
 import { Card } from "components/Card"
+import { ContainerCard } from "components/ContainerCard"
 
 export function LibraryScreen() {
   const navegate = useNavigate()
   const [profile, setProfile] = useState<IUser>()
+  const [cards, setCards] = useState<ILibrary[][]>([])
   const [cookies] = useCookies()
   const user: IUser = cookies?.user as IUser
 
@@ -21,10 +23,24 @@ export function LibraryScreen() {
     axios.get<IUser>(`${API_URL}/api/user/profile/${user.id}`)
       .then((response) => {
         setProfile(response.data)
+        if (response.data.libraries)
+          formatLibraries(response.data.libraries)
         return response.data
       })
       .catch((error) => console.error(error))
-  }, [user])
+  }, [user, navegate])
+
+  const formatLibraries = (cards: ILibrary[]) => {
+    const chunkSize = 3
+    const slicedData: ILibrary[][] = []
+
+    for (let i = 0; i < cards.length; i += chunkSize) {
+      slicedData.push(cards.slice(i, i + chunkSize))
+    }
+
+    setCards(slicedData)
+  }
+
 
   const handleRemoveLibrary = (e: any, id: number) => {
     axios.delete<IReview>(`${API_URL}/api/library/${id}`)
@@ -45,49 +61,50 @@ export function LibraryScreen() {
         <div className="col-12" id="content-main">
           <h1 className="title-h3"> Veja os conteúdos salvos!</h1>
 
-          {
-            profile?.libraries && profile.libraries.length > 0 ? (
-              profile.libraries.map((library: ILibrary) => (
-                <div className="row card-main" key={library.id}>
+          <div className="row card-main">
+            {cards.map((chuck: ILibrary[], index) => (
+              <ContainerCard key={index}>
+                {chuck.map((library: ILibrary) => (
+                  library ? (
+                    <Card
+                      image={library.content.image}
+                      key={library.content.id}
+                    >
+                      <div className="container-library-info">
+                        <h4 className="card-title">
+                          {library.content.name}
+                          <span className="card-text">
+                            Você parou no {library.episode} epísodio da season {library.seasonContent}
+                          </span>
 
-                  <Card
-                    image={library.content.image}
-                    key={library.content.id}
-                  >
-                    <div className="container-library-info">
-                      <h4 className="card-title">
-                        {library.content.name}
-                        <span className="card-text">
-                          Você parou no {library.episode} epísodio da season {library.seasonContent} de {library.content?.title}
-                        </span>
-
-                      </h4>
-                    </div>
-                    <p className="card-text">{library.content?.description}</p>
-                    <div className="row">
-                      <div className="col-6">
-
-                        <Link to={`/content/${library.content.id}`} className="form-control btn btn-primary">
-                          Ver mais
-                        </Link>
+                        </h4>
                       </div>
-                      <div className="col-6">
-                        <Button type="button" classBtn="btn btn-danger library-icon" onClick={(e) => handleRemoveLibrary(e, library.id)}>
-                          Deletar Item
-                        </Button>
-                      </div>
+                      <p className="card-text">{library.content?.description}</p>
+                      <div className="row">
+                        <div className="col-6">
 
+                          <Link to={`/content/${library.content.id}`} className="form-control btn btn-primary">
+                            Ver 
+                          </Link>
+                        </div>
+                        <div className="col-6">
+                          <Button type="button" classBtn="btn btn-danger library-icon" onClick={(e) => handleRemoveLibrary(e, library.id)}>
+                            Deletar
+                          </Button>
+                        </div>
+
+                      </div>
+                    </Card>
+                  ) : (
+                    <div className="d-flex flex-row">
+                      <h3 className="subtitle-lb"><strong>Não há conteúdo a ser exibido:</strong></h3>
+                      <Link to="/" className="nav-link">Voltar à Home</Link>
                     </div>
-                  </Card>
-                </div>
-              ))
-            ) : (
-              <div className="d-flex flex-row">
-                <h3 className="subtitle-lb"><strong>Não há conteúdo a ser exibido:</strong></h3>
-                <Link to="/" className="nav-link">Voltar à Home</Link>
-              </div>
-            )
-          }
+                  )
+                ))}
+              </ContainerCard>
+            ))}
+          </div>
         </div>
       </div>
     </div >
