e7196fa97c124bb907491dc8f5b59fc4dc1c0a16
Igor Meurer <Igorigormeurer@hotmail.com>
2023-06-26 17:48:27 -0300

    Algumas mudanças

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,9 @@
 import { useCookies } from "react-cookie"
+import { ReactNode, useEffect, useState } from "react"
+import axios from "axios"
+import { API_URL } from "types/apiURL"
 import { IUser } from "types"
+import { Form } from "components/Form"
 export function ProfileScreen() {
   const [cookies, setCookie] = useCookies()
   const user : IUser = cookies?.user as IUser
@@ -25,6 +29,15 @@ export function ProfileScreen() {
           <div className="col-xs-12 divider text-center">
             <div className="col-xs-12 col-sm-4 emphasis">
               <button type="button" onClick={handleForm} className="form-control btn btn-info btn-block">Editar perfil</button>
+            </div>
+            <div className="button-container">
+              <div className="selects-container">
+              <Form>
+                <div className="row">
+                  <div className="col-4 col-sm-8 d-inline-flex"></div>
+                </div>
+              </Form>
+              </div>
             </div>
           </div>
         </div>
