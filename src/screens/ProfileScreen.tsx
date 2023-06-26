fc18dee730580ddda74790e14f9f77bd92017260
Igor Meurer <Igorigormeurer@hotmail.com>
2023-06-26 17:10:58 -0300

    Arrumado a div de perfil

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -3,26 +3,28 @@ import { IUser } from "types"
 export function ProfileScreen() {
   const [cookies, setCookie] = useCookies()
   const user : IUser = cookies?.user as IUser
+  const handleForm = () => {
+    
+  }
   return(
     <div className="container-fluid">
-      <div className="row">
+      <div className="row-center" id="content-main">
       <div className="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
         <div className="profile-container">
           <div className="col-sm-12">
             <div className="col-xs-12 col-sm-8">
-            <h1>ProfileScreen</h1>
-            {cookies?.user && <h3>{user.nickname}</h3>}
+            <h3><p><strong>Nickname: </strong>{cookies?.user && user.nickname}</p></h3>
             <p><strong>Quantidade de reviews:</strong>{user.reviews?.length}</p>
             </div>
-            <div>
+            <div className="col-xs-12 col-sm-4 text-center">
               <figure>
-                <img src={user.image} alt="" className="img-fluid roundend img-profile"/>
+                <img src={user.image} className="img-circle img-responsive"/>
               </figure>
             </div>
           </div>
           <div className="col-xs-12 divider text-center">
             <div className="col-xs-12 col-sm-4 emphasis">
-              <button type="button" className="form-control btn btn-info btn-block">Editar perfil</button>
+              <button type="button" onClick={handleForm} className="form-control btn btn-info btn-block">Editar perfil</button>
             </div>
           </div>
         </div>
