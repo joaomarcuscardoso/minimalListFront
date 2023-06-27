73696a41b45d6092208c7b71cbab7c32e6959d4f
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2023-06-26 22:51:48 -0300

    Adicionando perfil

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./assets/css/global/styles.css"
 import { Route, Routes } from "react-router-dom"
 import { HomeScreen } from "screens/HomeScreen"
 import { ProfileScreen } from "screens/ProfileScreen"
-import { ReviewScreen } from "screens/ReviewScreen"
 import { LoginScreen } from "screens/LoginScreen"
 import { RegisterScreen } from "screens/RegisterScreen"
 import { ContentScreen } from "screens/ContentScreen"
@@ -20,7 +19,6 @@ function App() {
         <Route path="/profile" element={<ProfileScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
-        <Route path="/review" element={<ReviewScreen />} />
         <Route path="/library" element={<LibraryScreen />} />
         <Route path="/content/:id" element={<ContentScreen />} />
       </Routes>
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -6,42 +6,57 @@ import { IUser } from "types"
 import { Form } from "components/Form"
 export function ProfileScreen() {
   const [cookies, setCookie] = useCookies()
-  const user : IUser = cookies?.user as IUser
-  const handleForm = () => {
-    
+  let user: IUser = cookies?.user as IUser
+  const [img, setImg] = useState<string>(user.imagePathComplete)
+
+  const handleForm = (e: any) => {
+    const formData = new FormData()
+    formData.append("image", e.target.files[0])
+
+    axios.put<IUser>(`${API_URL}/api/user/profile/image/${user.id}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      }
+    })
+    .then((response) => {
+        if (response?.data?.imagePathComplete) {
+          setImg(response.data.imagePathComplete)
+          user = response.data
+          setCookie("user", user, { path: "/" })
+        }
+    })
+    .catch((error) => console.error(error))
   }
-  return(
+  return (
     <div className="container-fluid">
       <div className="row-center" id="content-main">
-      <div className="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-        <div className="profile-container">
-          <div className="col-sm-12">
-            <div className="col-xs-12 col-sm-8">
-            <h3><p><strong>Nickname: </strong>{cookies?.user && user.nickname}</p></h3>
-            <p><strong>Quantidade de reviews:</strong>{user.reviews?.length}</p>
-            </div>
-            <div className="col-xs-12 col-sm-4 text-center">
-              <figure>
-                <img src={user.image} className="img-circle img-responsive"/>
-              </figure>
-            </div>
-          </div>
-          <div className="col-xs-12 divider text-center">
-            <div className="col-xs-12 col-sm-4 emphasis">
-              <button type="button" onClick={handleForm} className="form-control btn btn-info btn-block">Editar perfil</button>
-            </div>
-            <div className="button-container">
-              <div className="selects-container">
-              <Form>
-                <div className="row">
-                  <div className="col-4 col-sm-8 d-inline-flex"></div>
+        <div className="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+          <div className="profile-container">
+            <div className="col-sm-12">
+              <div className="row">
+                <div className="col-xs-1 col-sm-1 text-center">
+                  <figure>
+                    <img src={img} alt={user.nickname} className="img-circle img-fluid" />
+                  </figure>
                 </div>
-              </Form>
+                <div className="col-xs-12 col-sm-8">
+                  <h3><p><strong>Nickname: </strong>{cookies?.user && user.nickname}</p></h3>
+                  <p><strong>Email: </strong>{cookies?.user && user.email}</p>
+                  <p><strong>Quantidade de reviews:</strong>{user.reviews?.length ?? 0}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-12 divider text-center">
+              <div className="button-container">
+                <div className="selects-container">
+                  <Form>
+                    <input type="file" name="image" onChange={handleForm} />
+                  </Form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   )
